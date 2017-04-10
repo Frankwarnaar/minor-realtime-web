fabf66b515a9e5497f44f23231d5d2eca45f6b39
Frank Warnaar <f.warnaar@viriciti.com>
2017-04-10 11:14:21 +0200

    Code style applied

--- src/server/app.js
+++ src/server/app.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const express = require('express');
 const path = require('path');
+const express = require('express');
 const compression = require('compression');
 const staticAsset = require('static-asset');
 const io = require('socket.io');
 
 const app = express();
 const port = process.env.PORT || 3000;
-const host = process.env.HOST ||'0.0.0.0';
+const host = process.env.HOST || '0.0.0.0';
 const baseDir = 'build/client';
 
 app.engine('ejs', require('express-ejs-extend'));
@@ -23,9 +23,9 @@ app.use(express.static(baseDir, {
 }));
 
 app.get('/', (req, res) => {
-    res.render('index');
+	res.render('index');
 });
 
-app.listen(port, host, (err) => {
+app.listen(port, host, err => {
 	err ? console.error(err) : console.log(`app running on http://localhost:${port}`);
 });
